2b4af1b50d8eb15691bba24ce5b2371c2427ea3d
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2024-06-09 13:09:13 +0900

    Create App resource

---
+++ app.tf
@@ -0,0 +1,24 @@
+resource "google_cloud_run_v2_service" "app" {
+  name     = "app"
+  location = "asia-northeast1"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_policy" "policy" {
+  location    = "asia-northeast1"
+  name        = google_cloud_run_v2_service.app.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
